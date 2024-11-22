a7f0f6fb2a062ea57653fe69b1abb3debe7b6a9a
Philani Mhlongo <mhlongophilani04@gmail.com>
2024-11-22 15:47:36 +0200

    The Add idempotence Test

--- orders/.aws-sam/build/AddOrderFunction/create_order.py
+++ orders/.aws-sam/build/AddOrderFunction/create_order.py
@@ -4,11 +4,19 @@ from decimal import Decimal
 import json
 import uuid
 from datetime import datetime
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.idempotency import (
+    IdempotencyConfig, DynamoDBPersistenceLayer, idempotent_function
+)
 
-# Globals
 orders_table = os.getenv('TABLE_NAME')
+idempotency_table = os.getenv('IDEMPOTENCY_TABLE_NAME')
 dynamodb = boto3.resource('dynamodb')
 
+persistence_layer = DynamoDBPersistenceLayer(table_name=idempotency_table)
+idempotency_config = IdempotencyConfig(event_key_jmespath="powertools_json(body).orderId")
+
+@idempotent_function(data_keyword_argument="event", config=idempotency_config, persistence_store=persistence_layer)
 def add_order(event: dict):
     detail = json.loads(event['body'])
     restaurant_id = detail['restaurantId']
@@ -17,7 +25,7 @@ def add_order(event: dict):
     user_id = event['requestContext']['authorizer']['claims']['sub']
     order_time = datetime.strftime(datetime.utcnow(), '%Y-%m-%dT%H:%M:%SZ')
     order_id = detail['orderId']
-
+    
     ddb_item = {
         'orderId': order_id,
         'userId': user_id,
@@ -31,21 +39,24 @@ def add_order(event: dict):
             'orderTime': order_time,
         }
     }
+    
     ddb_item = json.loads(json.dumps(ddb_item), parse_float=Decimal)
-
     table = dynamodb.Table(orders_table)
-    # We must use conditional expression, otherwise put_item will always replace the original order and will never fail
-    table.put_item(Item=ddb_item, ConditionExpression='attribute_not_exists(orderId) AND attribute_not_exists(userId)')
-
+    
+    # We must use conditional expression to prevent race conditions
+    table.put_item(
+        Item=ddb_item,
+        ConditionExpression='attribute_not_exists(orderId) AND attribute_not_exists(userId)'
+    )
+    
     detail['orderId'] = order_id
     detail['orderTime'] = order_time
     detail['status'] = 'PLACED'
-
     return detail
 
-
-def lambda_handler(event, context):
-    """Handles the lambda method invocation"""
+def lambda_handler(event, context: LambdaContext):
+    idempotency_config.register_lambda_context(context)
+    
     try:
         order_detail = add_order(event=event)
         response = {
